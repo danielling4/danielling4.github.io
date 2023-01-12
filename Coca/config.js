d7ec36624c3dd906d24ac1a6936823ca32715082
Daniel Ling <40855271+danielling4@users.noreply.github.com>
2023-01-12 22:37:15 +0000

    Add files via upload

--- Coca/config.js
+++ Coca/config.js
@@ -16,17 +16,18 @@ var config = {
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
-            id: 'slug-style-id',
+            id: 'in-the-jungle',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
-            image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            title: 'Introduction',
+            image: 'C:\Users\DanielLing\Desktop\Misc Vizzes\Cocaine Viz\storytelling-main\storytelling-main\src\images\Coca leaves washed out.jpg',
+            description: 'Cocaine is a stimulant drug that is made from the leaves of the South American Coca plant.The following visualisation highlights some of the economics involved with converting the leaves of the Coca plant into Cocaine powder and the vast sums of money involved along the way.',
+
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+               center: [-72.80983, 0.47814],
+zoom: 11.79,
+pitch: 49.00,
+bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -46,17 +47,17 @@ var config = {
             ]
         },
         {
-            id: 'second-identifier',
+            id: 'to-the-lab',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
+            title: 'Jungle Production',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Coca leaves are shredded and formed into a paste which is then mixed and processed with a variety of chemicals to product Cocaine powder..',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: -43.2,
+                center: [-73.05118, 0.52017],
+                zoom: 11.79,
+                pitch: 55.50,
+                bearing: 0.00,
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -71,17 +72,17 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'third-identifier',
+            id: 'to-the ports',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: 'To the Ports',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Cocaine Powder is smuggled out of Columbia in many different ways with the container ships using the port of Barranquilla a popular choice for the Cartels.',
             location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
-                pitch: 8.01,
-                bearing: 0.00
+                center: [-74.75979, 10.96337],
+                zoom: 14.37,
+                pitch: 71.50,
+                bearing: -59.20
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
